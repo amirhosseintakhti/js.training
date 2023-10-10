211b057afa6b4892d1364a369c179abbd281b32f
AmirHossein Takhti <amirhosseintakhti.at@gmail.com>
2023-10-10 16:06:21 +0330

    first

---
+++ kertop-js/kertop-js/app.js
@@ -0,0 +1,11 @@
+let variable="21";
+console.log(variable);
+console.log(typeof variable);
+let a = Number(variable);
+console.log(a);
+console.log(typeof a)
+const car = ["saab","volvo","bmw"]
+document.getElementById("demo").innerHTML = car
+console.log(car)
+console.log(typeof car);
+
